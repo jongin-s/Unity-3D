26dce81c83885b4deaac86bad79d553fc79c931a
JongIn Kim <ppj7515@gmail.com>
2024-05-26 22:35:25 +0900

    점수 표시

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    void OnParticleCollision(GameObject other) 
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
     {
-        Debug.Log($"{name}I'm hit! by {other.gameObject.name}");
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+
 }
